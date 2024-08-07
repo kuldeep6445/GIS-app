4acf3873fa66c4b7b50783df31d61326bb0edbb7
kuldeep6445 <kuldeepbhogal111@gmail.com>
2024-08-07 18:06:59 +0530

    initialised project

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    geom = models.PointField()
+
+    def __str__(self) -> str:
+        return self.name
